c652ac5ac55bb8013d0eaadee45d4fb6091144ed
szymonstuszek <szymonstuszek@gmail.com>
2021-03-11 10:42:27 +0100

    Test push after fork

--- src/main/java/guru/sfg/beer/order/service/services/BeerOrderServiceImpl.java
+++ src/main/java/guru/sfg/beer/order/service/services/BeerOrderServiceImpl.java
@@ -101,7 +101,7 @@ public class BeerOrderServiceImpl implements BeerOrderService {
         throw new RuntimeException("Customer Not Found");
     }
 
-    @Override
+    @Override//test
     public BeerOrderDto getOrderById(UUID customerId, UUID orderId) {
         return beerOrderMapper.beerOrderToDto(getOrder(customerId, orderId));
     }
